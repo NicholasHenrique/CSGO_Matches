7b92b7c07408b3e94be4602ac0f246a3910833f9
NicholasHenrique <nicholas.henrique96@gmail.com>
2024-11-15 01:14:16 -0300

    fix: timestamp columns to correct format and bronze database using hive

--- match_details_stream.py
+++ match_details_stream.py
@@ -2,6 +2,14 @@
 como o match_stream.py salva na bronze partidas únicas, então de acordo com o funcionamento do get_match_details.py,
 não haverá partidas duplicadas em raw/matches_proceeded e, portanto, não haverá partidas duplicadas em raw/tb_maps e raw/tb_leaderboards,
 logo não há a necessidade de criar uma window para que só pegue as partidas únicas em raw/tb_maps e raw/tb_leaderboards
+entretanto quando tira a window, dá o erro:
+pyspark.errors.exceptions.captured.UnsupportedOperationException: [DELTA_MULTIPLE_SOURCE_ROW_MATCHING_TARGET_ROW_IN_MERGE] Cannot perform Merge as multiple source rows matched and attempted to modify the same
+target row in the Delta table in possibly conflicting ways. By SQL semantics of Merge,
+when multiple source rows match on the same target row, the result may be ambiguous
+as it is unclear which source row should be used to update or delete the matching
+target row. You can preprocess the source table to eliminate the possibility of
+multiple matches. Please refer to
+https://docs.delta.io/latest/delta-update.html#upsert-into-a-table-using-merge
 """
 
 import delta
@@ -9,7 +17,7 @@ import os
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType, BooleanType, FloatType
 from pyspark.sql import SparkSession, window
 from pyspark.sql import functions as F
-from os.path import abspath
+from pyspark.sql.functions import col, to_timestamp, regexp_replace
 
 def upsertToDelta(df, batchId):
   '''
@@ -17,28 +25,32 @@ def upsertToDelta(df, batchId):
   The GameId is used as a group by and UpdatedUtc is used as an order by.
   If it's found a duplicated match, the duplicate will be not be saved.
   '''
-  windowSpec = window.Window.partitionBy("GameId").orderBy(F.lit(1)) # .orderBy(F.lit(1)) .orderBy("UpdatedUtc")
+  windowSpec = window.Window.partitionBy("GameId").orderBy(col("UpdatedUtc").desc()) #.orderBy(F.lit(1)) # .orderBy(F.lit(1)) .orderBy("UpdatedUtc")
   df_new = df.withColumn("row_number", F.row_number().over(windowSpec)).filter("row_number = 1")
+  df_new = df_new.withColumn("Day", to_timestamp(regexp_replace("Day", 'T', ' '), 'yyyy-MM-dd HH:mm:ss')) \
+                   .withColumn("DateTime", to_timestamp(regexp_replace("DateTime", 'T', ' '), 'yyyy-MM-dd HH:mm:ss')) \
+                   .withColumn("Updated", to_timestamp(regexp_replace("Updated", 'T', ' '), 'yyyy-MM-dd HH:mm:ss')) \
+                   .withColumn("UpdatedUtc", to_timestamp(regexp_replace("UpdatedUtc", 'T', ' '), 'yyyy-MM-dd HH:mm:ss'))
 
   ( bronzeDeltaTable.alias("bronze")
                     .merge(df_new.alias("raw"), "bronze.GameId = raw.GameId")
+                    # .merge(df.alias("raw"), "bronze.GameId = raw.GameId")
                     .whenMatchedUpdateAll()
                     .whenNotMatchedInsertAll()
                     .execute()
   )
 
-warehouse_location = abspath('spark-warehouse').replace('c', 'C')
+warehouse_location = os.path.abspath('spark-warehouse')
 
 builder = SparkSession.builder \
     .master('local[*]') \
+    .config("spark.sql.warehouse.dir", warehouse_location) \
     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
-    # .config("spark.sql.warehouse.dir", warehouse_location) \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+    .enableHiveSupport()
 
 spark = delta.configure_spark_with_delta_pip(builder) \
         .getOrCreate()
-
-# spark.conf.set("spark.local.dir", "/tmp/spark-temp")
 
 leaderboards_schema = StructType([
     StructField("PlayerId", IntegerType(), False),
@@ -93,8 +105,7 @@ SCHEMAS = {
   "tb_maps": map_schema
 }
 
-if "spark-warehouse" not in os.listdir():
-  spark.sql("CREATE DATABASE bronze")
+spark.sql("CREATE DATABASE IF NOT EXISTS bronze")
 
 try:
   for TABLE_NAME in list(SCHEMAS.keys()):
@@ -103,36 +114,35 @@ try:
     '''
     if TABLE_NAME not in os.listdir('spark-warehouse/bronze.db'):
       df = spark.read.parquet(f"raw/{TABLE_NAME}")
-      windowSpec = window.Window.partitionBy("GameId").orderBy(F.lit(1)) # .orderBy(F.lit(1)) .orderBy("UpdatedUtc")
+      windowSpec = window.Window.partitionBy("GameId").orderBy(col("UpdatedUtc").desc()) #.orderBy(F.lit(1)) # .orderBy(F.lit(1)) .orderBy("UpdatedUtc")
       df_new = df.withColumn("row_number", F.row_number().over(windowSpec)).filter("row_number = 1").drop("row_number")
 
-      # TODO: verificar o que está acontecendo aqui, provavelmente não está sendo possível salvar as novas tabelas dentro do database bronze, que provavelmente este seja o erro do projeto que me travou e me fez parar
+      # df.write.mode("overwrite").format("delta").saveAsTable(f"bronze.{TABLE_NAME}")
+      df_new = df_new.withColumn("Day", to_timestamp(regexp_replace("Day", 'T', ' '), 'yyyy-MM-dd HH:mm:ss')) \
+                   .withColumn("DateTime", to_timestamp(regexp_replace("DateTime", 'T', ' '), 'yyyy-MM-dd HH:mm:ss')) \
+                   .withColumn("Updated", to_timestamp(regexp_replace("Updated", 'T', ' '), 'yyyy-MM-dd HH:mm:ss')) \
+                   .withColumn("UpdatedUtc", to_timestamp(regexp_replace("UpdatedUtc", 'T', ' '), 'yyyy-MM-dd HH:mm:ss'))
+      df_new.write.mode("overwrite").format("delta").saveAsTable(f"bronze.{TABLE_NAME}")
+    else:
+      bronzeDeltaTable = delta.tables.DeltaTable.forName(spark, f"bronze.{TABLE_NAME}")
 
-      # df_new.write.mode("overwrite").format("delta").save(f"bronze.{TABLE_NAME}") # overwriting it's not overwrititng because it creates a different file name
-      df_new.write.mode("overwrite").format("delta").save(f"spark-warehouse/bronze.db/{TABLE_NAME}")
-      # df_new.write.mode("overwrite").format("delta").save(f"{warehouse_location}\\bronze.db\\{TABLE_NAME}")
-      # df_new.write.format("delta").saveAsTable(name=f"{warehouse_location}.bronze.{TABLE_NAME}", mode="overwrite")
-      # df_new.write.mode("overwrite").format("delta").saveAsTable(f"bronze.{TABLE_NAME}")
+      '''
+      When new matches lands in raw, a stream is responsible for saving these new matches in bronze.
+      '''
+      df_stream = ( spark.readStream
+                        .format("parquet")
+                        .schema(SCHEMAS[TABLE_NAME])
+                        .load(f"raw/{TABLE_NAME}")
+                  )
 
-    bronzeDeltaTable = delta.tables.DeltaTable.forPath(spark, f"spark-warehouse/bronze.db/{TABLE_NAME}") #"bronze"
+      stream = ( df_stream.writeStream
+                          .foreachBatch(upsertToDelta)
+                          .option("checkpointLocation", f"spark-warehouse/bronze.db/{TABLE_NAME}_checkpoint")
+                          .outputMode("update")
+                          .start()
+              )
 
-    '''
-    When new matches lands in raw, a stream is responsible for saving these new matches in bronze.
-    '''
-    df_stream = ( spark.readStream
-                      .format("parquet")
-                      .schema(SCHEMAS[TABLE_NAME])
-                      .load(f"raw/{TABLE_NAME}")
-                )
-
-    stream = ( df_stream.writeStream
-                        .foreachBatch(upsertToDelta)
-                        .option("checkpointLocation", f"spark-warehouse/bronze.db/{TABLE_NAME}_checkpoint")
-                        .outputMode("update")
-                        .start()
-            )
-
-    stream.processAllAvailable()
-    stream.stop()
+      stream.processAllAvailable()
+      stream.stop()
 finally:
   spark.stop()
