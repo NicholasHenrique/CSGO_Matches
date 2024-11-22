516b3d34a95fb23fc698d35c83be3793c36c2fea
NicholasHenrique <nicholas.henrique96@gmail.com>
2024-11-21 23:39:13 -0300

    chore/fix: code identation / update upsertToDelta definition, remove window and filter table for timestamp columns transformation

--- match_details_stream.py
+++ match_details_stream.py
@@ -1,44 +1,29 @@
-"""
-como o match_stream.py salva na bronze partidas únicas, então de acordo com o funcionamento do get_match_details.py,
-não haverá partidas duplicadas em raw/matches_proceeded e, portanto, não haverá partidas duplicadas em raw/tb_maps e raw/tb_leaderboards,
-logo não há a necessidade de criar uma window para que só pegue as partidas únicas em raw/tb_maps e raw/tb_leaderboards
-entretanto quando tira a window, dá o erro:
-pyspark.errors.exceptions.captured.UnsupportedOperationException: [DELTA_MULTIPLE_SOURCE_ROW_MATCHING_TARGET_ROW_IN_MERGE] Cannot perform Merge as multiple source rows matched and attempted to modify the same
-target row in the Delta table in possibly conflicting ways. By SQL semantics of Merge,
-when multiple source rows match on the same target row, the result may be ambiguous
-as it is unclear which source row should be used to update or delete the matching
-target row. You can preprocess the source table to eliminate the possibility of
-multiple matches. Please refer to
-https://docs.delta.io/latest/delta-update.html#upsert-into-a-table-using-merge
-"""
-
 import delta
 import os
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType, BooleanType, FloatType
-from pyspark.sql import SparkSession, window
+from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
-from pyspark.sql.functions import col, to_timestamp, regexp_replace
+from pyspark.sql.functions import to_timestamp, regexp_replace
+from functools import partial
 
-def upsertToDelta(df, batchId):
-  '''
-  In order to guarantee there aren't any duplicated matches, a Window is used to filter matches based on its GameId and UpdatedUtc.
-  The GameId is used as a group by and UpdatedUtc is used as an order by.
-  If it's found a duplicated match, the duplicate will be not be saved.
-  '''
-  windowSpec = window.Window.partitionBy("GameId").orderBy(col("UpdatedUtc").desc()) #.orderBy(F.lit(1)) # .orderBy(F.lit(1)) .orderBy("UpdatedUtc")
-  df_new = df.withColumn("row_number", F.row_number().over(windowSpec)).filter("row_number = 1")
-  df_new = df_new.withColumn("Day", to_timestamp(regexp_replace("Day", 'T', ' '), 'yyyy-MM-dd HH:mm:ss')) \
-                   .withColumn("DateTime", to_timestamp(regexp_replace("DateTime", 'T', ' '), 'yyyy-MM-dd HH:mm:ss')) \
-                   .withColumn("Updated", to_timestamp(regexp_replace("Updated", 'T', ' '), 'yyyy-MM-dd HH:mm:ss')) \
-                   .withColumn("UpdatedUtc", to_timestamp(regexp_replace("UpdatedUtc", 'T', ' '), 'yyyy-MM-dd HH:mm:ss'))
+def upsertToDelta(df, batchId, bronzeDeltaTable, table_name, on_fields):
+    '''
+    Upsert function to insert and update matches details. It also transforms tb_leaderboards' timestamp columns into valid formats.
+    '''
+    if table_name == "tb_leaderboards":
+        df = df.withColumn("Day", to_timestamp(regexp_replace("Day", 'T', ' '), 'yyyy-MM-dd HH:mm:ss')) \
+                .withColumn("DateTime", to_timestamp(regexp_replace("DateTime", 'T', ' '), 'yyyy-MM-dd HH:mm:ss')) \
+                .withColumn("Updated", to_timestamp(regexp_replace("Updated", 'T', ' '), 'yyyy-MM-dd HH:mm:ss')) \
+                .withColumn("UpdatedUtc", to_timestamp(regexp_replace("UpdatedUtc", 'T', ' '), 'yyyy-MM-dd HH:mm:ss'))
 
-  ( bronzeDeltaTable.alias("bronze")
-                    .merge(df_new.alias("raw"), "bronze.GameId = raw.GameId")
-                    # .merge(df.alias("raw"), "bronze.GameId = raw.GameId")
-                    .whenMatchedUpdateAll()
-                    .whenNotMatchedInsertAll()
-                    .execute()
-  )
+    join_condition = " and ".join([f"bronze.{field} = raw.{field}" for field in on_fields])
+
+    ( bronzeDeltaTable.alias("bronze")
+                      .merge(df.alias("raw"), join_condition)
+                      .whenMatchedUpdateAll()
+                      .whenNotMatchedInsertAll()
+                      .execute()
+    )
 
 warehouse_location = os.path.abspath('spark-warehouse')
 
@@ -107,42 +92,50 @@ SCHEMAS = {
 
 spark.sql("CREATE DATABASE IF NOT EXISTS bronze")
 
-try:
-  for TABLE_NAME in list(SCHEMAS.keys()):
+table_on_fields = {
+    "tb_leaderboards": ["PlayerId", "GameId", "MapName"], 
+    "tb_maps": ["Number", "GameId"]
+}
+
+for TABLE_NAME in list(SCHEMAS.keys()):
     '''
     Full load
     '''
     if TABLE_NAME not in os.listdir('spark-warehouse/bronze.db'):
-      df = spark.read.parquet(f"raw/{TABLE_NAME}")
-      windowSpec = window.Window.partitionBy("GameId").orderBy(col("UpdatedUtc").desc()) #.orderBy(F.lit(1)) # .orderBy(F.lit(1)) .orderBy("UpdatedUtc")
-      df_new = df.withColumn("row_number", F.row_number().over(windowSpec)).filter("row_number = 1").drop("row_number")
-
-      # df.write.mode("overwrite").format("delta").saveAsTable(f"bronze.{TABLE_NAME}")
-      df_new = df_new.withColumn("Day", to_timestamp(regexp_replace("Day", 'T', ' '), 'yyyy-MM-dd HH:mm:ss')) \
-                   .withColumn("DateTime", to_timestamp(regexp_replace("DateTime", 'T', ' '), 'yyyy-MM-dd HH:mm:ss')) \
-                   .withColumn("Updated", to_timestamp(regexp_replace("Updated", 'T', ' '), 'yyyy-MM-dd HH:mm:ss')) \
-                   .withColumn("UpdatedUtc", to_timestamp(regexp_replace("UpdatedUtc", 'T', ' '), 'yyyy-MM-dd HH:mm:ss'))
-      df_new.write.mode("overwrite").format("delta").saveAsTable(f"bronze.{TABLE_NAME}")
+        df = spark.read.schema(SCHEMAS[TABLE_NAME]).parquet(f"raw/{TABLE_NAME}")
+        '''
+        Transformation of tb_leaderboards' timestamp columns into valid formats.
+        '''
+        if TABLE_NAME == "tb_leaderboards":
+            df = df.withColumn("Day", to_timestamp(regexp_replace("Day", 'T', ' '), 'yyyy-MM-dd HH:mm:ss')) \
+                        .withColumn("DateTime", to_timestamp(regexp_replace("DateTime", 'T', ' '), 'yyyy-MM-dd HH:mm:ss')) \
+                        .withColumn("Updated", to_timestamp(regexp_replace("Updated", 'T', ' '), 'yyyy-MM-dd HH:mm:ss')) \
+                        .withColumn("UpdatedUtc", to_timestamp(regexp_replace("UpdatedUtc", 'T', ' '), 'yyyy-MM-dd HH:mm:ss'))
+        df.write.mode("overwrite").format("delta").saveAsTable(f"bronze.{TABLE_NAME}")
     else:
-      bronzeDeltaTable = delta.tables.DeltaTable.forName(spark, f"bronze.{TABLE_NAME}")
+        bronzeDeltaTable = delta.tables.DeltaTable.forName(spark, f"bronze.{TABLE_NAME}")
 
-      '''
-      When new matches lands in raw, a stream is responsible for saving these new matches in bronze.
-      '''
-      df_stream = ( spark.readStream
-                        .format("parquet")
-                        .schema(SCHEMAS[TABLE_NAME])
-                        .load(f"raw/{TABLE_NAME}")
-                  )
+        '''
+        When new matches land in raw, a stream is responsible for saving these new matches in bronze.
+        '''
+        df_stream = ( spark.readStream
+                            .format("parquet")
+                            .schema(SCHEMAS[TABLE_NAME])
+                            .load(f"raw/{TABLE_NAME}")
+                    )
 
-      stream = ( df_stream.writeStream
-                          .foreachBatch(upsertToDelta)
-                          .option("checkpointLocation", f"spark-warehouse/bronze.db/{TABLE_NAME}_checkpoint")
-                          .outputMode("update")
-                          .start()
-              )
+        upsertToDeltaFunc = partial(upsertToDelta, bronzeDeltaTable=bronzeDeltaTable, table_name=TABLE_NAME, on_fields=table_on_fields[TABLE_NAME])
 
-      stream.processAllAvailable()
-      stream.stop()
-finally:
-  spark.stop()
+        stream = ( df_stream.writeStream
+                            .foreachBatch(upsertToDeltaFunc)
+                            .option("checkpointLocation", f"spark-warehouse/bronze.db/{TABLE_NAME}_checkpoint")
+                            .outputMode("update")
+                            .start()
+                )
+
+        '''
+        Processing in batch with stream
+        '''
+        stream.processAllAvailable()
+        stream.stop()
+spark.stop()
